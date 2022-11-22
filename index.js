b8102d5e917e33632195053ad8cf36b8e8304d26
apuntesnavarrete <apuntesnavarrete@gmail.com>
2022-11-21 20:52:06 -0600

    operadores

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
-alert("hola")
+console.log("inicio operadores de asignacion")
+let x = 10;
+console.log(x)
+x += 5;
+console.log(x)
 
-/*practicar JavaScript Assignment*/
+let text = "Hello";
+console.log(text)
+text += " World";
+console.log(text)
+
+var age = 19
+
+//pensar en ejercicios 
+
+//and , y 
+//debe cumplir ambas condiciones
+if (age >= 18 && age <=30){
+    console.log("edad permitida")
+}else{
+    console.log("edad no permitida")
+}
+//debe cumplir una sola condicion
+if (age >= 18 || age <=30){
+    console.log("edad permitida")
+}else{
+    console.log("edad no permitida")
+}
+
