866ddf9567b162744fe5165b5df0a41f75f00be5
apuntesnavarrete <apuntesnavarrete@gmail.com>
2022-11-22 15:22:42 -0600

    22-nov

---
+++ veintiuno.js
@@ -0,0 +1,20 @@
+//Juego de cartas 21
+
+// Maquina
+
+// Jugador yo
+
+//2 cartas cada quien , con puntajes del 1 al 10 (hay mas cartas)
+
+
+
+console.log("HOLI")
+
+let mazo = (1,2,3,4,5,6,7,8,9)
+
+let num_1 = Math.floor(Math.random() * 10);
+let num_2 = Math.floor(Math.random() * 10);
+let num_3 = Math.floor(Math.random() * 10);
+
+
+console.log(`${num_1 ,num_2 , num_3`)
