ad97148b7b18bdb5b0456e5c65b9daa733b87b38
apuntesnavarrete <apuntesnavarrete@gmail.com>
2022-11-28 15:14:10 -0600

    slice ejercicio

--- slice.js
+++ slice.js
@@ -2,3 +2,16 @@ let texto = "hola compas voy a cortar este string"
 let corte = texto.slice(5,8)
 
 console.log(corte)
+
+let nombres = ["juan","pedro","pablo"]
+
+console.log(nombres)
+
+nombre_1 = nombres[0].slice(0,4) + "@heroesfb.com"
+nombre_2 = nombres[1].slice(0,4) + "@heroesfb.com"
+nombre_3 = nombres[2].slice(0,4) + "@heroesfb.com"
+
+
+console.log(nombre_1)
+console.log(nombre_2)
+console.log(nombre_3)
