6c5f3b1370482218b35385c1503bfe66de9e7b44
apuntesnavarrete <apuntesnavarrete@gmail.com>
2022-12-05 17:43:16 -0600

    filter

---
+++ arrays/filter.js
@@ -0,0 +1,12 @@
+const numbers = [45, 4, 9, 16, 25];
+console.log(numbers)
+numbers.filter(myFunction);
+
+
+
+
+
+function myFunction(value, index, array) {
+  console.log(value)
+  console.log(index)
+}
