314864b145346386ec96ce2cd24ec0c4c6932ad8
apuntesnavarrete <apuntesnavarrete@gmail.com>
2022-12-06 20:43:43 -0600

    some

--- arrays/every.js
+++ arrays/every.js
@@ -1,16 +1,37 @@
+const numbers = [16, 17, 15, 14];
+
+
 //un grupo de amigos iran a un bar , todos deben ser mayores de edad
 //el grupo puede entrar al bar?
 
-const numbers = [19, 20, 21, 23];
+
+/*
 let allOver18 = numbers.every(myFunction);
+*/
+
+let allOver18 = numbers.some(myFunction);
+
+//un grupo de amigos iran a un zologico , debe a ver al menos una persona con ine
+//el grupo puede entrar al zoo?
+
+
 
 
 if (allOver18 == true){
-    console.log("Bienvenidos al bar ")
+    console.log("Bienvenidos al zoo ")
 } else {
-    console.log("Hay menores entre ustedes o los dejan afuera o no buscan otro lugar")
+    console.log("no tienen un mayor con ine")
 }
 
 function myFunction(value, index, array) {
   return value > 18;
 }
+
+
+/*
+if (allOver18 == true){
+    console.log("Bienvenidos al bar ")
+} else {
+    console.log("Hay menores entre ustedes o los dejan afuera o no buscan otro lugar")
+}
+*/
