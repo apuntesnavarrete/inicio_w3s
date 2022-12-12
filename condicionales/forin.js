2018ef2d2e68e59d3f2684186424f61ed1139914
apuntesnavarrete <apuntesnavarrete@gmail.com>
2022-12-11 21:47:50 -0600

    forin forof

---
+++ condicionales/forin.js
@@ -0,0 +1,16 @@
+let numbers = [3,5,7,9,10];
+
+/*
+for(let number of numbers) {
+  console.log("El numero a imprimir es")
+  console.log(number)
+}
+*/
+for(let number in numbers) {
+  console.log("El id a imprimir es")
+  console.log(number)
+  console.log("El Numero a imprimir es")
+
+  console.log(numbers[number])
+
+}
